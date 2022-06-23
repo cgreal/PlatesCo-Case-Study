51c710ac73b669d3fcc5ce841e3adc3b4e7e0e9c
cagrierol <cagrierol@neredekal.com>
2022-06-23 18:53:25 +0300

    plates-co-case-study

---
+++ src/assets/scripts/app.js
@@ -0,0 +1,155 @@
+// SELECT ELEMENTS
+const productsEl = document.querySelector(".products");
+const cartItemsEl = document.querySelector(".cart-items");
+const subtotalEl = document.querySelector(".subtotal");
+const totalItemsInCartEl = document.querySelector(".total-items-in-cart");
+
+// RENDER PRODUCTS
+function renderProdcuts() {
+  products.forEach((product) => {
+    productsEl.innerHTML += `
+            <div class="item">
+                <div class="item-container">
+                    <div class="item-img">
+                        <img src="${product.imgSrc}" alt="${product.name}">
+                    </div>
+                    <div class="desc">
+                        <h2>${product.name}</h2>
+                        <h2><small>$</small>${product.price}</h2>
+                        <p>
+                            ${product.description}
+                        </p>
+                        <button
+                            type="button"
+                            class="add-to-cart-btn"
+                            onclick="addToCart(${product.id})"
+                        >
+                            Add to Cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `;
+  });
+}
+renderProdcuts();
+
+// cart array
+let cart = JSON.parse(localStorage.getItem("CART")) || [];
+updateCart();
+
+// ADD TO CART
+function addToCart(id) {
+  // check if prodcut already exist in cart
+  if (cart.some((item) => item.id === id)) {
+    changeNumberOfUnits("plus", id);
+  } else {
+    const item = products.find((product) => product.id === id);
+    cart.push({
+      ...item,
+      numberOfUnits: 1,
+    });
+    console.log("ne", cart);
+  }
+
+  updateCart();
+}
+
+// update cart
+function updateCart() {
+  renderCartItems();
+  renderSubtotal();
+
+  // save cart to local storage
+  localStorage.setItem("CART", JSON.stringify(cart));
+}
+
+// calculate and render subtotal
+function renderSubtotal() {
+  let totalPrice = 0,
+    totalItems = 0;
+
+  cart.forEach((item) => {
+    console.log(item);
+    if (item.id === 0 && item.numberOfUnits > 0) {
+      totalPrice -=
+        ((item.numberOfUnits - (item.numberOfUnits % 2)) / 2) *
+        (item.price / 2);
+    }
+
+    totalPrice += item.price * item.numberOfUnits;
+    totalItems += item.numberOfUnits;
+  });
+
+  // add to order delivery price
+  if (totalItems > 0) {
+    if (totalPrice < 90 && totalPrice >= 50) {
+      totalPrice += 2.95;
+    } else if (totalPrice < 50) {
+      totalPrice += 4.95;
+    }
+  }
+  // for total price rounding regulation
+  // totalPrice += 0.001 - (totalPrice % 0.001);
+  subtotalEl.innerHTML = `Subtotal (${totalItems} items): $${(
+    Math.floor(totalPrice * 100) / 100
+  ).toFixed(2)}`;
+  totalItemsInCartEl.innerHTML = totalItems;
+}
+
+// render cart items
+function renderCartItems() {
+  cartItemsEl.innerHTML = ""; // clear cart element
+  cart.forEach((item) => {
+    if (item.numberOfUnits > 0) {
+      console.log(item.imgSrc);
+      cartItemsEl.innerHTML += `
+      <div class="cart-item">
+          <div class="item-info" onclick="removeItemFromCart(${item.id})">
+              <img src="${item.imgSrc}" alt="${item.name}">
+              <h4>${item.name}</h4>
+          </div>
+          <div class="unit-price">
+              <small>$</small>${item.price}
+          </div>
+          <div class="units">
+              <div class="btn minus" onclick="changeNumberOfUnits('minus', ${item.id})">-</div>
+              <div class="number">${item.numberOfUnits}</div>
+              <div class="btn plus" onclick="changeNumberOfUnits('plus', ${item.id})">+</div>           
+          </div>
+      </div>
+    `;
+    }
+  });
+}
+
+// remove item from cart
+function removeItemFromCart(id) {
+  cart = cart.filter((item) => item.id !== id);
+
+  updateCart();
+}
+
+// change number of units for an item
+function changeNumberOfUnits(action, id) {
+  cart = cart.map((item, i) => {
+    let numberOfUnits = item.numberOfUnits;
+    if (item.id === id) {
+      if (action === "minus" && numberOfUnits > 0) {
+        numberOfUnits--;
+      } else if (action === "plus" && numberOfUnits < item.instock) {
+        numberOfUnits++;
+      } else if (action === "minus" && numberOfUnits === 1) {
+        numberOfUnits--;
+        removeItemFromCart(id);
+      }
+    }
+
+    return {
+      ...item,
+      numberOfUnits,
+    };
+  });
+
+  updateCart();
+}
---
+++ src/assets/scripts/product-lists.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    id: 0,
+    name: "Red Plate - R01",
+    price: 32.95,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./src/assets/images/R01.jpg",
+  },
+  {
+    id: 1,
+    name: "Green Plate - G01",
+    price: 24.95,
+    instock: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./src/assets/images/G01.jpg",
+  },
+  {
+    id: 2,
+    name: "Blue Plate - B01",
+    price: 7.95,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./src/assets/images/B01.jpg",
+  },
+];
